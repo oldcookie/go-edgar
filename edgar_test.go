b986fb0831a49bad72a9cb3f2f869f1246bcf104
oldcookie <steve@lazarentertainment.com>
2016-05-26 16:03:20 -0400

    removed handler and used channels instead

--- edgar_test.go
+++ edgar_test.go
@@ -1,51 +1,50 @@
 package edgar
 
 import (
-  "time"
-  "testing"
-  "strings"
+	"strings"
+	"testing"
+	"time"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 const teststring = "03 ENTERTAINMENT GROUP INC:0001322914:\n033 ASSET MANAGEMENT LLC /:0001114831:"
 
 func TestGetCIKList(t *testing.T) {
-  ciks := []string{"0001322914", "0001114831"}
-  names := []string{"03 ENTERTAINMENT GROUP INC", "033 ASSET MANAGEMENT LLC /"}
+	var ciks = []string{"0001322914", "0001114831"}
+	var names = []string{"03 ENTERTAINMENT GROUP INC", "033 ASSET MANAGEMENT LLC /"}
 
-  if testing.Short() {
-    t.Skip("Skipping actual download and parsing of CIK file list in shor mode")
-  } else {
-    noCallHandlerFn := func (e *CIKEntry, l int) error {
-      t.Errorf("This handler shouldn't be called")
-      return nil
-    }
+	if testing.Short() {
+		t.Skip("Skipping actual download and parsing of CIK file list in shor mode")
+	} else {
+		entries, err := GetCIKList(time.Now(), 100)
+		if err != nil {
+			t.Error(err)
+		}
+		for entry := range entries {
+			t.Error("This should never be reached", entry)
+		}
 
-    code, err := GetCIKList(noCallHandlerFn, time.Now())
-    if code != 304 {
-      t.Errorf("Status code should be 304 but got: %d, %s", code, err)
-    }
-
-    handlerFn := func (e *CIKEntry, l int) error {
-      if e.cik != ciks[l] || e.name != names[l] {
-        t.Errorf("Results didn't match - Line %d: %s, %s - Got %s, %s",
-          l, ciks[l], names[l], e.cik, e.name )
-      }
-      return nil
-    }
-    parseCIKList(strings.NewReader(teststring), handlerFn)
-  }
+		ch := make(chan *CIKEntry, 10)
+		parseCIKList(strings.NewReader(teststring), ch)
+		close(ch)
+		for e := range ch {
+			if e.cik != ciks[e.lineNo] || e.name != names[e.lineNo] {
+				t.Errorf("Results didn't match - Line %d: %s, %s - Got %s, %s",
+					e.lineNo, ciks[e.lineNo], names[e.lineNo], e.cik, e.name)
+			}
+		}
+	}
 }
 
 func TestGetCompany(t *testing.T) {
-  c, err := GetCompany("AAPL")
-  if err != nil {
-    t.Error(err)
-  }
-  assert := assert.New(t)
+	c, err := GetCompany("AAPL")
+	if err != nil {
+		t.Error(err)
+	}
+	assert := assert.New(t)
 
-  t.Log(c)
-  assert.Equal(c.Info.Cik, "0000320193", "CIK should be the same")
-  assert.Equal(c.Info.Name, "APPLE INC", "Name should be the APPLE INC")
+	t.Log(c)
+	assert.Equal(c.Info.Cik, "0000320193", "CIK should be the same")
+	assert.Equal(c.Info.Name, "APPLE INC", "Name should be the APPLE INC")
 }
